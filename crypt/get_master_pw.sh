c4cd3d86d4be6136ceea2a35d8190972c4bfd108
arctic-hen7 <arctic.hen@pm.me>
2025-12-30 12:00:56 +1100

    feat: added basic master pw caching with `keyctl`

--- crypt/get_master_pw.sh
+++ crypt/get_master_pw.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-# Gets the master password by prompting the user.
+# Gets the master password by prompting the user, with kernel keyring caching.
 #
 # Requirements:
 #   - $ACE_KEYRING_DIR
 #   - $ACE_PINENTRY
+#   - keyctl (from keyutils package)
+#
+# To manually clear the cache: keyctl purge user ace-master-pw
+
+# Cache timeout in minutes
+CACHE_TIMEOUT_MINUTES=15
 
 MAX_TRIES=3
+CACHE_KEY_NAME="ace-master-pw"
 
 master_pw_file="$ACE_KEYRING_DIR/master.enc"
 if [ ! -f "$master_pw_file" ]; then
@@ -13,6 +20,26 @@ if [ ! -f "$master_pw_file" ]; then
     exit 1
 fi
 
+# Try to retrieve from kernel keyring cache
+set +e
+key_id=$(keyctl search @s user "$CACHE_KEY_NAME" 2>/dev/null)
+keyctl_search_code=$?
+set -e
+
+if [ $keyctl_search_code -eq 0 ]; then
+    # Key found in cache, retrieve it
+    set +e
+    master_pw=$(keyctl print "$key_id" 2>/dev/null)
+    keyctl_print_code=$?
+    set -e
+
+    if [ $keyctl_print_code -eq 0 ]; then
+        echo "$master_pw"
+        exit 0
+    fi
+fi
+
+# Not in cache, prompt user
 num_tries=0
 while [ $num_tries -lt $MAX_TRIES ]; do
     if [ $num_tries -gt 0 ]; then
@@ -31,6 +58,16 @@ while [ $num_tries -lt $MAX_TRIES ]; do
         master_pw="$(openssl enc -d -in "$master_pw_file" -aes-256-cbc -salt -pbkdf2 -pass fd:3 3<<<"$pass")"
 
         if [ $? -eq 0 ]; then
+            # Successfully decrypted, store in kernel keyring with timeout
+            set +e
+            key_id=$(echo -n "$master_pw" | keyctl padd user "$CACHE_KEY_NAME" @s 2>/dev/null)
+            if [ $? -eq 0 ]; then
+                # Set timeout in seconds
+                timeout_seconds=$((CACHE_TIMEOUT_MINUTES * 60))
+                keyctl timeout "$key_id" "$timeout_seconds" 2>/dev/null
+            fi
+            set -e
+
             echo "$master_pw"
             exit 0
         else
